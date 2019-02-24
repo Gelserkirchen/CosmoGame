acde716420ecae2c462ca34e5ae7417cb9ea82e3
Andrew Tolstykh <gelserkirchen@mail.ru>
2019-02-24 17:15:02 +0500

    Initial Commit

---
+++ CosmoGameTests/CosmoGameTests.swift
@@ -0,0 +1,34 @@
+//
+//  CosmoGameTests.swift
+//  CosmoGameTests
+//
+//  Created by Andrew on 24/02/2019.
+//  Copyright © 2019 Andrew. All rights reserved.
+//
+
+import XCTest
+@testable import CosmoGame
+
+class CosmoGameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
